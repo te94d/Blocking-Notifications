e2a862de44b73e3992f8bbb8136fae3756f927d1
te94d <te94da@gmail.com>
2022-11-10 20:11:20 +0900

    v2

--- extension/js/content.js
+++ extension/js/content.js
@@ -1,13 +1,13 @@
 Notification = (function(Notification) {
+  console.log("point-0")
   function MyNotification(...args) {
-      console.log("title: ", args[0]);
-      console.log("body: ", args[1].body);
-      console.log("icon: ", args[1].icon);
+    console.log("point-1")
+    console.log("title: ", args[0]);
+    console.log("body: ", args[1].body);
+    console.log("icon: ", args[1].icon);
   };
-
+  console.log("point-2")
   Object.assign(MyNotification, Notification);
   MyNotification.prototype = Notification.prototype;
-
   return MyNotification;
 })(Notification);
-console.log("end");
