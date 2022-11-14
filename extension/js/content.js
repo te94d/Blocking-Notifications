a3b1176ea87a83f94e8a4e2fc14d79879fb79261
te94d <te94da@gmail.com>
2022-11-14 13:56:15 +0900

    test

--- extension/js/content.js
+++ extension/js/content.js
@@ -1,8 +1,35 @@
+const black_list = "rules.json";
+
 Notification = (function(Notification) {
   function MyNotification(...args) {
     console.log("title: ", args[0]);
     console.log("body: ", args[1].body);
     console.log("icon: ", args[1].icon);
+
+    
+    const zod = "zodiac.json";    // 読み込むJSONファイル
+    
+    // 12星座情報を整形して表示する
+    function formatJSON(json){
+        console.log(json);
+    
+        // JSONファイルを整形して表示
+        for(let zodiac of json){
+            console.log("ja" + zodiac.ja)
+            console.log("latin" + zodiac.latin)
+        }
+        document.getElementById("result").innerHTML = html;
+    }
+    
+    // 起動時の処理
+    window.addEventListener("load", ()=>{
+    
+        // 12星座情報を取得して表示
+        fetch(zod)
+            .then( response => response.json())
+            .then( data => formatJSON(data));
+    
+    });
 
     if (args[0] === "ブラックリストのキーワード") {
       console.log("keyword blocked!");
@@ -30,7 +57,3 @@ window.open = function (open) {
 
   };
 }(window.open);
-
-navigator.serviceWorker.getRegistrations().then(function(registrations) {
-  document.querySelector('#status').textContent = 'ServiceWorkerRegistrations が見つかりました。';
-});
